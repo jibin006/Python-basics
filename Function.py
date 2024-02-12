ff4303ebea07aee64b7fdb703000fa8ff2eed79d
JIBIN_BENNY <146290857+jibin006@users.noreply.github.com>
2024-02-12 13:03:10 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,15 @@
+
+
+def Greatest(num1,num2):
+    if (num1 > num2):
+        return num1
+    else:
+        return num2
+
+num1= int(input("Enter the value1: "))
+num2= int(input("Enter the value2: "))
+ 
+GreatestValue = Greatest(num1,num2)
+print(GreatestValue)    
+    
+    
